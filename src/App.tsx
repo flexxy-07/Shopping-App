f4a39e39f221de1de0176103b88214129a3b67e6
Sumiitt-007 <sumitrpt007@gmail.com>
2025-08-18 23:54:06 +0530

    Created Sidebar and Created Redux like Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./App.css"
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Sidebar from './Components/Sidebar'
+import MainComponent from './Components/MainContent'
+import MainContent from './Components/MainContent'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <div className='flex h-screen'>
+        <Sidebar/>
+      </div>
+
+      <div className="rounded w-full flex justify-between flex-wrap">
+        <Routes>
+          <Route path='/' element={<MainContent/>}/>
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
