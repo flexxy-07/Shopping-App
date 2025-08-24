aeb644ce871e7b90da1fac4efca5dcf076f24b3a
Sumiitt-007 <sumitrpt007@gmail.com>
2025-08-24 23:35:28 +0530

    Completed the functioning of the MainContent

--- src/Components/MainContent.tsx
+++ src/Components/MainContent.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useFilter } from './FilterContext'
 import { Tally3 } from 'lucide-react';
+import axios from 'axios';
 
 const MainContent = () => {
   const{
@@ -8,22 +9,76 @@ const MainContent = () => {
     selectedCategory,
     minPrice,
     maxPrice,
-    keyword
+    keyword,
   } = useFilter();
 
   const[products, setProducts] = useState<any[]>([]);
   const[filter, setFilter] = useState('all');
   const[currentPage, setCurrentPage] = useState(1);
-  const[dropDown, setDropDown] = useState(false);
+  const[dropDownOpen, setDropDownOpen] = useState(true);
   const itemsPerPage = 12;
+
+  useEffect(() => {
+    let url =  `https://dummyjson.com/products?limit=${itemsPerPage}&skip=${(currentPage - 1) * itemsPerPage}`
+
+    if (keyword) {
+      url = `https://dummyjson.com/products/search?q=${keyword}` 
+    }
+    axios.get(url).then(response  => {
+      setProducts(response.data.products);
+    }).catch(error => {
+      console.error('Error Fetching data',error);
+    })
+  },[currentPage, keyword]);
+
+  const getFilteredProducts = () => {
+    let filteredProducts = products;
+
+    if(selectedCategory){
+      filteredProducts = filteredProducts.filter(
+        (product) => product.category === selectedCategory
+      );
+    }
+
+    if(minPrice !== undefined){
+      filteredProducts = filteredProducts.filter(product => product.price >= minPrice )
+    }
+
+    if (maxPrice !== undefined) {
+      filteredProducts = filteredProducts.filter
+      (product => product.price <= maxPrice)
+    }
+
+    if (searchQuery) {
+      filteredProducts = filteredProducts.filter(
+        product => product.title.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    }
+
+    switch (filter) {
+      case "expensive":
+        return filteredProducts.sort((a,b) => b.price - a.price)
+    
+      case "cheap" :
+        return filteredProducts.sort((a,b) => a.price - b.price)
+
+      case "popular" : 
+        return filteredProducts.sort((a,b) => b.rating - a.rating)
+
+        default :
+        return filteredProducts;
+    }
+  }
+
+  getFilteredProducts();
 
   return (
     <section className='xl:w-[55rem] lg:w-[55rem] sm:w-[40rem] xs:w-[20rem] p-5'>
       <div className="mb-5"> 
         <div className="flex flex-col sm:flex-row justify-between items-center">
           <div className="flex justify-start items-start">
-            <button className='border px-4 py-2       rounded-full flex items-center'>
-              <Tally3 className='mr-2 '/>
+            <button className='border px-4 py-2 rounded-full flex items-center'>
+              <Tally3 className='mr-2'/>
 
               {filter === 'all'
                ? 'Filter'
@@ -31,9 +86,38 @@ const MainContent = () => {
                }
             </button>
 
-            {dropDownOpen}
+            {dropDownOpen && (
+              <div
+              className='absolute bg-white border border-gray-300 rounded mt-11 w-full sm:w-40'
+              >
+                <button
+                  onClick={() => setFilter('cheap')}
+                  className='block px-4 py-2 w-full text-left hover:bg-gray-200'                 
+                >
+                Cheap
+                </button>
+
+                <button
+                  onClick={() => setFilter('expensive')}
+                  className='block px-4 py-2 w-full text-left hover:bg-gray-200'                 
+                >
+                Expensive
+                </button>
+
+                <button
+                  onClick={() => setFilter('popular')}
+                  className='block px-4 py-2 w-full text-left hover:bg-gray-200'                 
+                >
+                  Popular
+                </button>
+              </div>
+            )}
           </div>
         </div>
+
+          <div className="grid grid-cols sm:grid-cols md:grid-cols-4 gap-5">
+            {/* BookCard */}
+          </div>
       </div>
     </section>
   )
